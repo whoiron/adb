79ef46342fd972cc447911b8488c3a65f7f167ce
whoiron <36080118+whoiron@users.noreply.github.com>
2018-02-17 02:29:02 +0900

    general discord追加、TB宣伝サイトを分ける (#6)

--- src/pages/discord.js
+++ src/pages/discord.js
@@ -11,13 +11,32 @@ export default ({data}) => (
     <table>
       <thead>
       <tr>
+        <th>Discord</th>
+        <th>情報</th>
+      </tr>
+      </thead>
+      <tbody>
+      {data.allDiscordYaml.edges.filter(({node}) => node.type === "general").map(({node}, i) => (
+        <tr key={`${i}`}>
+          <td>{node.link && <a href={node.link} target="_blank">{node.id}</a>}</td>
+          <td>
+            {node.description}
+          </td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+
+    <table>
+      <thead>
+      <tr>
         <th>アイドル</th>
         <th>Discord</th>
         <th>サイト</th>
       </tr>
       </thead>
       <tbody>
-      {data.allDiscordYaml.edges.filter(({node}) => node.type !== "ex").map(({node}, i) => (
+      {data.allDiscordYaml.edges.filter(({node}) => node.type === null).map(({node}, i) => (
         <tr key={`${i}`}>
           <td>
             {/*<Link to={node.fields.character___slug}>*/}
@@ -32,6 +51,16 @@ export default ({data}) => (
                 <li><a href={link} target="_blank">{name}</a></li>
               ))}
             </ul>
+            }
+            {node.tbSites && node.tbSites.length > 0 &&
+            <div>
+              <span>TB宣伝</span>
+              <ul>
+                {node.tbSites.map(({name, link}) => (
+                  <li><a href={link} target="_blank">{name}</a></li>
+                ))}
+              </ul>
+            </div>
             }
           </td>
         </tr>
@@ -64,6 +93,16 @@ export default ({data}) => (
               ))}
             </ul>
             }
+            {node.tbSites && node.tbSites.length > 0 &&
+            <div>
+              <span>TB宣伝</span>
+              <ul>
+                {node.tbSites.map(({name, link}) => (
+                  <li><a href={link} target="_blank">{name}</a></li>
+                ))}
+              </ul>
+            </div>
+            }
           </td>
         </tr>
       ))}
@@ -81,7 +120,12 @@ export const discordQuery = graphql`
           link
           character
           type
+          description
           sites {
+            name
+            link
+          }
+          tbSites {
             name
             link
           }
